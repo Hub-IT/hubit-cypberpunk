80c8dd8666e2df2b46a1777f6aae283e44f0e15a
Rizart Dokollari <r.dokollari@gmail.com>
2015-04-24 03:24:20 +0000

    Prepare repo for heroku deployment.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>HubIt | CyberPunks</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
+				<div class="title">HubIt | CyberPunks</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
